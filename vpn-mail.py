f9c4b5b5bd3976ea150b6ff286ebe54b238446b6
BlackCoderZed <41987824+BlackCoderZed@users.noreply.github.com>
2021-07-11 09:59:11 +0630

    Add files via upload

---
+++ vpn-mail.py
@@ -0,0 +1,48 @@
+import email, smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+subject = "OpenVPN"
+body = "Automated email"
+sender_email = "blackcoder.zed@gmail.com"
+receiver_email = input("Enter receiver's email : ")
+filename = input("Enter filename : ")
+
+password = input("Type your password and press enter:")
+
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = receiver_email
+message["Subject"] = subject
+message["Bcc"] = receiver_email
+
+# Add body to email
+message.attach(MIMEText(body, "plain"))
+
+with open(filename, "rb") as attachment:
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {filename}",
+)
+
+# Add attachment to message and convert message to string
+message.attach(part)
+text = message.as_string()
+
+# Log in to server using secure context and send email
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, text)
+print('Success')
+            
